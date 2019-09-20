4d7a425903d43909f6c41da634bd9deb399f30ab
Calvin Tan <hello@calvin-tan.com>
2019-09-20 14:57:06 +0800

    Update page name

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,9 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Home</router-link>
       <router-link to="/about">About</router-link>
+      <router-link to="/directory">Directory</router-link>
     </div>
     <router-view />
   </div>
@@ -17,6 +18,7 @@
   text-align: center;
   color: #2c3e50;
 }
+
 #nav {
   padding: 30px;
 }
--- src/views/Landing.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <div class="landing">
-    <h1>This is the landing page</h1>
-  </div>
-</template>

901a462f447cafa8dee79560bcbae4b834322f4e
Sally Li <sallyli0410@gmail.com>
2019-09-20 14:00:40 +0800

    started on homepage

---
+++ src/components/PopularBldg.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="popular-bldg">
+    <div class="container">
+    <h1>Uplift Helps You Find The Perfect Lift To Advertise</h1>
+    <div class="row d-flex">
+      <div class="col-4">
+        <div class="card">
+
+        </div>
+      </div>
+      <div class="col-4">
+        <div class="card">
+
+        </div>
+      </div>
+      <div class="col-4">
+        <div class="card">
+
+        </div>
+      </div>
+    </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default{
+    name: "PopularBldg"
+  };
+</script>
+
+<style scoped>
+.popular-bldg{
+  background:yellow;
+  height:100%;
+}
+.card{
+  background: white;
+  border-radius: 10px;
+  height:100px;
+}
+</style>

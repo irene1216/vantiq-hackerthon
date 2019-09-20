e5732d07b83c16739e71f33bb17b00fd8726f463
Irene Chan <irene.chan.16@outlook.com>
2019-09-20 16:53:15 +0800

    Formated data with area name

--- src/components/Directory.vue
+++ src/components/Directory.vue
@@ -6,7 +6,8 @@
           <b-card-text>Number of floors: {{lift.Floor_Count}}</b-card-text>
           <b-card-text>{{lift.Housing_Estate_Name}}</b-card-text>
           <b-card-text>{{lift.lift_Name}}</b-card-text>
-          <b-card-text>{{lift.Lift_Provider_Code}}</b-card-text>
+          <b-card-text>{{lift.lift.city}}</b-card-text>
+          <b-card-text>{{lift.lift.town}}</b-card-text>
 
         </b-card>
       </div>
@@ -33,11 +34,42 @@ export default {
   },
   methods: {
     fetchData() {
-      console.log("in directory")
       axios.get('https://dev.vantiq.cn/api/v1/resources/custom/BaseInfoForHackathon?token=B__1eBCT0MZJFtJkyS1Y_dcFMcuB8O0Tuc77fKYegqs=').then(response => {
-        console.log(response.data.slice(0, 10));
-        this.lifts = response.data.slice(0, 10)
+        let array = response.data.slice(0, 10)
+        this.formatLocation(array)
       })
+    },
+    formatLocation(array) {
+      console.log("formating")
+      console.log(array)
+      let tmp = []
+      array.forEach((lift)=>{
+        lift.city = "Shanghai"
+        if (lift.Town_Code === "310114"){
+          tmp.push({
+            id: lift.id,
+            registerId: lift._id,
+            building: lift.Housing_Estate_Name,
+            floors: lift.Floor_Count,
+            city: "Shanghai",
+            town: "JiaDing",
+            address: lift.lift_Name,
+            geo: lift.Lift_Geo
+          })
+        } else if (lift.Town_Code === "310109") {
+          tmp.push({
+            id: lift.id,
+            registerId: lift._id,
+            building: lift.Housing_Estate_Name,
+            floors: lift.Floor_Count,
+            city: "Shanghai",
+            town: "HongKou",
+            address: lift.lift_Name,
+            geo: lift.Lift_Geo
+          })
+        }
+      })
+      this.lifts = tmp
     },
   }
 };

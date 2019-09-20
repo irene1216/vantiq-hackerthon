b252f94ba0e8c5f97d3cdca8a2a5e4ad881ecd19
Irene Chan <irene.chan.16@outlook.com>
2019-09-20 15:21:19 +0800

    Creating the card component

---
+++ src/components/cards.vue
@@ -0,0 +1,18 @@
+<template>
+
+</template>
+<script>
+export default {
+  name: "Cards",
+  props: {
+    buildingName: String,
+    floors: Number,
+    City: String,
+    District: String,
+    liftsNo: Number
+  }
+}
+</script>
+<style>
+
+</style>
